143548deddd7d109335ad2906f66836f5579b289
Adam Leuer <aleuer@thoughtworks.com>
2022-04-21 23:14:46 -0700

    Initial commit

---
+++ StarCitizenKeymapVisualizer/Program.cs
@@ -0,0 +1,10 @@
+﻿using SFML.Window;
+
+var window = new Window(new VideoMode(1920, 1080, 30), "Star Citizen Keymap Visualizer");
+
+while (true)
+{
+    window.DispatchEvents();
+    window.Display();
+    Thread.Sleep(TimeSpan.FromMilliseconds(8));
+}
