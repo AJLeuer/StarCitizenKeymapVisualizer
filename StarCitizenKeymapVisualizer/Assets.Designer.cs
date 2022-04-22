ea182a031c85c016f5f3f8f68866f28a6ffb1ee9
Adam Leuer <aleuer2@illinois.edu>
2022-04-22 01:07:22 -0700

    Added Orbitron font file

--- StarCitizenKeymapVisualizer/Assets.Designer.cs
+++ StarCitizenKeymapVisualizer/Assets.Designer.cs
@@ -69,5 +69,15 @@ namespace StarCitizenKeymapVisualizer {
                 return ((byte[])(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] Orbitron {
+            get {
+                object obj = ResourceManager.GetObject("Orbitron", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
     }
 }
